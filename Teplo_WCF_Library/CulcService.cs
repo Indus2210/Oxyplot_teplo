c970f6777b9b56a546f9e856423e57320dd086e0
indus2210 <indus2210@yandex.ru>
2018-10-31 20:40:41 +0300

    Добавил математику и сервесную службу

---
+++ Teplo_WCF_Library/CulcService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TeploLibrary;
+
+namespace Teplo_WCF_Library
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class CulcService : ICulcService
+    {
+        public OutputDate SumMatrixes(InputDate inputMatrixes)
+        {
+            // inputMatrixes.Mass_a
+            OutputDate mass_data = new OutputDate();
+            int a = inputMatrixes.Mass_u.GetLength(0);
+            int b = inputMatrixes.Mass_u.GetLength(1);
+            double h = inputMatrixes.H;
+            double time = inputMatrixes.Time;
+            double tau = inputMatrixes.Tau;
+            
+            mass_data.Culc_Teplo = new double[a, b];
+            Teplo teplo = new Teplo();
+            mass_data.Culc_Teplo = teplo.PoslCulc(inputMatrixes.Mass_u, time, tau, h);
+
+            return mass_data;
+        }
+    }
+}
---
+++ Teplo_WCF_Library/ICulcService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Teplo_WCF_Library
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface ICulcService
+    {
+        [OperationContract]
+        OutputDate SumMatrixes(InputDate inputMatrixes);
+
+    }
+    [DataContract]
+    public class InputDate
+    {
+        double[,] mass_u;
+        double time;
+        double h;
+        double tau;
+
+        [DataMember]
+        public double[,] Mass_u
+        {
+            get { return mass_u; }
+            set { mass_u = value; }
+        }
+
+        [DataMember]
+        public double Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+
+        [DataMember]
+        public double H
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        [DataMember]
+        public double Tau
+        {
+            get { return tau; }
+            set { tau = value; }
+        }
+
+    }
+
+    [DataContract]
+    public class OutputDate
+    {
+        double[,] culc;
+
+        [DataMember]
+        public double[,] Culc_Teplo
+        {
+            get { return culc; }
+            set { culc = value; }
+        }
+    }
+}
