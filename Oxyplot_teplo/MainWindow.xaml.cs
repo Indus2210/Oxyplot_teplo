951b6befcc5dc68798dbc1ed6cda0f7f25587c05
juliapopova <44148548+juliapopova@users.noreply.github.com>
2018-11-14 23:42:20 +0300

    добавлен класс Paint

--- Oxyplot_teplo/MainWindow.xaml.cs
+++ Oxyplot_teplo/MainWindow.xaml.cs
@@ -20,9 +20,73 @@ namespace Oxyplot_teplo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double[,] _U;
+        private Rectangle[,] _Rect;
+        private int _sizeX;
+        private int _sizeY;
+        private double _h;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PrepearPaint();
+        }
+
+        private void SetStartingValue()
+        {
+            
+
+        }
+
+        public void PrepearPaint()
+        {
+            double h = 0.01;
+            double [,]u = new double[10,10];
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    u[i, j] = 1;
+
+            Painter painter = new Painter();
+            painter.PrepearDrow(front_canvas, h, 10, 10);
+            painter.DrowTemp(u);
+            //System.Windows.Shapes.Rectangle rect;
+            //rect = new System.Windows.Shapes.Rectangle();
+            //rect.Fill = new SolidColorBrush(Colors.Black);
+            //rect.Width = 1;
+            //rect.Height = 1;
+            //Canvas.SetLeft(rect, 0);
+            //Canvas.SetTop(rect, 0);
+            //front_canvas.Children.Add(rect);
+        //    int n = 100;
+        //    SolidColorBrush brush = new SolidColorBrush();
+        //    brush.Color = Colors.Red;
+        //    Rectangle[,] rects = new Rectangle[n, n];
+        //    double clr;
+        //    Color color;
+
+        
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        for (int j = 0; j < n; j++)
+        //        {
+        //            clr = (u[i, j] * 255) / 500;
+        //            color = Color.FromRgb((byte)clr, 0, (byte)(255 - (int)clr));
+        //            brush = new SolidColorBrush(color);
+        //            rects[i, j] = new Rectangle();
+        //            rects[i, j].Fill = brush;
+        //            rects[i, j].Width = 10;
+        //            rects[i, j].Height = 10;
+        //            Canvas.SetLeft(rects[i, j], i * 10);
+        //            Canvas.SetTop(rects[i, j], j * 10);
+        //            front_canvas.Children.Add(rects[i, j]);
+        //        }
+        //    }
+        //    front_canvas.InvalidateVisual();
+
         }
     }
 }
---
+++ Oxyplot_teplo/Painter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Oxyplot_teplo
+{
+    public class Painter
+    {
+        //private double[,] _U;
+        private Rectangle[,]_Rect;
+        //private int _sizeX;
+        //private int _sizeY;
+        //private double _h;
+
+        public Painter()
+        {
+            
+        }
+
+        public void DrowTemp(double [,]u)
+        {
+            double uMax, uMin;
+            uMax = 500;
+            uMin = 0;
+
+            int nX = u.GetLength(0);
+            int nY = u.GetLength(1);
+
+            for (int i = 0; i < nX; i++)
+                for (int j = 0; j < nY; j++)
+                {
+                    Rectangle rect = _Rect[i, j];
+                    byte r, g, b;
+                    r = Convert.ToByte(GetColor(u[i, j], uMin, uMax));
+                    g = 0;
+                    b = r;
+                    (rect.Fill as SolidColorBrush).Color = Color.FromRgb(r, g, b);
+                }
+
+        }
+        private double GetColor(double x, double uMin, double uMax)
+        {
+            
+
+            double d1 = 0, d2 = 255;
+
+            return ((x - uMin) * (d2 - d1)) / (uMax - uMin) + d1;
+        }
+
+        public void PrepearDrow(Canvas canvas, double h, int sizeX, int sizeY)
+        {
+
+            SolidColorBrush brush = new SolidColorBrush();
+            brush.Color = Colors.Red;
+            Rectangle [,]_Rect = new Rectangle[sizeX, sizeY];
+
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    //clr = (u[i, j] * 255) / 500;
+                    //color = Color.FromRgb((byte)clr, 0, (byte)(255 - (int)clr));
+                    brush = new SolidColorBrush();
+                    _Rect[i, j] = new Rectangle();
+                    _Rect[i, j].Fill = brush;
+                    _Rect[i, j].Width = 10;
+                    _Rect[i, j].Height = 10;
+                    Canvas.SetLeft(_Rect[i, j], i * 10);
+                    Canvas.SetTop(_Rect[i, j], j * 10);
+                    canvas.Children.Add(_Rect[i, j]);
+                }
+            }
+            
+        }
+
+    }
+}
